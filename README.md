18181aa3123c26c91a6e93512242b2ea4e9962f5
MarcosBernal <marcosbernal3@hotmail.com>
2018-11-08 10:18:26 +0100

    Add additional way of execute example. Improve README

--- README.md
+++ README.md
@@ -4,6 +4,22 @@ Couple of lines in spark to read from quijote.txt and count number of *Quijote* 
 
 It saves the expected result: **2167** in the relative path: **occurences_quijote/csv/\*.csv**.
 
+# Use
+
+Local execution:
+- `spark-submit --master local --class com.example.QuijoteCount target/scala-2.11/sparkquijotecount_2.11-1.0.jar`
+
+It reads from the relative path from the default configured file system:
+- in case of local fs: **./quijote.txt**
+- in case of HDFS: **hdfs:///\<defaultPath\>/quijote.txt**
+
+The results will be stored in:
+- in case of local fs: **./occurences_quijote/csv/\*.csv**
+- in case of HDFS: **hdfs:///\<defaultPath\>/occurences_quijote/csv/\*.csv**
+
+Additionally two parameters can be given to specify different paths. For instance with this call:
+- `spark-submit --master <MASTER_URL> --class com.example.QuijoteCount <YOUR_PATH>/sparkquijotecount_2.11-1.0.jar <YOUR_PATH>/quijote.txt <YOUR_DESTINY_PATH>`
+
 ## Files
 
 - [src/main/scala/com/example/QuijoteCount.scala](https://github.com/MarcosBernal/SparkQuijoteCount/blob/master/src/main/scala/com/example/QuijoteCount.scala): contains the lines of the word count program
--- src/main/scala/com/example/QuijoteCount.scala
+++ src/main/scala/com/example/QuijoteCount.scala
@@ -5,15 +5,19 @@ import org.apache.spark.sql.SparkSession
 
 object QuijoteCount {
   def main(args: Array[String]) {
+
+    val inputFile = if (args.length != 2) "quijote.txt" else args(0)
+    val outputFolder = if (args.length != 2) "occurences_quijote" else args(1)
+
     val spark = SparkSession.builder.getOrCreate
     import spark.implicits._
 
-    val quijoteDS: Dataset[String] = spark.read.textFile("quijote.txt")
+    val quijoteDS: Dataset[String] = spark.read.textFile(inputFile) // Optimal form spark.read.textFile(inputFile).flatMap(_.split("[\\W]+")).filter(_.matches("Quijote")).count
     val quijoteWords: Dataset[String] = quijoteDS.flatMap(_.split("[\\W]+")).filter(_.matches("[\\w]+")) // \W matches all NON alphanumeric and underscore chars
     val quijoteWordOccurrences: Long = quijoteWords.filter(_.matches("Quijote")).count                   // \w matches all alphanumeric and underscore chars
 
     val quijoteDF: DataFrame = Seq(quijoteWordOccurrences).toDF 
-    quijoteDF.coalesce(1).write.save("occurences_quijote/parquet")
-    quijoteDF.coalesce(1).write.format("csv").save("occurences_quijote/csv")
+    quijoteDF.coalesce(1).write.save(outputFolder + "/parquet")
+    quijoteDF.coalesce(1).write.format("csv").save(outputFolder + "/csv")
   }
 }
